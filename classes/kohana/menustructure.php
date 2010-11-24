d2292566c3de46783559b619f8ac456ea78886b1
ivansf <ivansotof@gmail.com>
2010-11-23 20:34:03 -0700

    fixed ORM support and added options

--- classes/kohana/menustructure.php
+++ classes/kohana/menustructure.php
@@ -4,15 +4,18 @@ abstract class Kohana_MenuStructure {
 
 	protected $html;
 	protected $items;
+	protected $options;
 	/**
 	 * Creates a new MenuStructure object.
 	 *
 	 * @param   array  configuration
+	 * @options array
+	 * 		link_prepend string - string goes before the uri.
 	 * @return  MenuStructure
 	 */
-	public static function factory($items)
+	public static function factory($items, $options = array())
 	{
-		return new MenuStructure($items);
+		return new MenuStructure($items, $options);
 	}
 
 
@@ -23,13 +26,20 @@ abstract class Kohana_MenuStructure {
 	 * @param  $config
  	 *
 	 */
-	protected function __construct($items) {
+	protected function __construct($items, $options) {
 		$html = '';
 		$root = 0;
+		$this->options = $options;
 
-		if (!is_array($items))
-			$this->items = (array)$items;
-		else
+		if (!is_array($items)) {
+			$this->items = $items;
+			// Assuming the menu is an ORM object
+			$items_holder = array();
+			foreach ($this->items as $i)
+				$items_holder[] = array('id' => $i->id, 'parent_id' => $i->parent_id, 'title' => $i->title,
+					'link' => $i->link);
+			$this->items = $items_holder;
+		} else
 			$this->items = $items;
 		
 		foreach ( $this->items as $item )
@@ -39,8 +49,7 @@ abstract class Kohana_MenuStructure {
 		$parent = $root;
 		$stack = array();
 
-		// HTML wrapper for the menu (open)
-		$html .= '<ul>'  . "\n";
+		$html .= '<ul>'  . "\n"; // opening the list
 
 		while ( $loop && ( ( $option = each( $children[$parent] ) ) || ( $parent > $root ) ) ) {
 			if (!$option) {
@@ -56,17 +65,33 @@ abstract class Kohana_MenuStructure {
 				$html .= '<li>' . $this->_build_link($option['value']['link'], $option['value']['title']) .
 						'</li>' . "\n";
 		}
-		$html .= '</ul>';
+		$html .= '</ul>'; // opening the list
 		$this->html = $html;
 	}
 
+	/**
+	 * Gets the menu in html ready to render.
+	 *
+	 * @return string
+	 */
 	function get_menu() {
 		return $this->html;
 	}
 
+	/**
+	 * Builds the anchor to be inserted in the link.
+	 * It will prepend a string if passed in the options
+	 *
+	 * @param string $link
+	 * @param string $title
+	 * @return string
+	 */
 	function _build_link($link = '', $title = '') {
+		// checking if we want to append something to the link
+		if (isset($this->options['link_prepend']))
+			$link = $this->options['link_prepend'] . $link;
+		
 		return Html::anchor($link, $title);
-		//return '<a href="' . $link . '">' . $title .'</a>';
 	}
 
 
